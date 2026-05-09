5007137dda8d5841eb785358e7c820363f12759b
Arfantta <ohmygenius6@gmail.com>
2026-05-09 18:25:47 +0700

    menambhakan function button

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const inputTanggal = document.getElementById("inputTanggal");
 const inputStatus = document.getElementById("inputStatus");
 const btnTambah = document.getElementById("btnTambah");
 const daftarTugas = document.getElementById("daftarTugas");
+
+btnTambah.addEventListener("click", function(){
+    
+})
