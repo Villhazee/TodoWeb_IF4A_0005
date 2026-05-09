90403ebf1f62b2e98b3eb97c39e8523e684b0293
Arfantta <ohmygenius6@gmail.com>
2026-05-09 19:35:54 +0700

    membaut function input tugas tanggal status

--- script.js
+++ script.js
@@ -4,6 +4,25 @@ const inputStatus = document.getElementById("inputStatus");
 const btnTambah = document.getElementById("btnTambah");
 const daftarTugas = document.getElementById("daftarTugas");
 
-btnTambah.addEventListener("click", function(){
-    
-})
+btnTambah.addEventListener("click", function () {
+
+    let teksTugas = inputTugas.value;
+    let tanggalTugas = inputTanggal.value;
+    let statusTugas = inputStatus.value;
+
+    if (teksTugas === "" || tanggalTugas === "") {
+        alert("Semua data harus diisi!");
+        return;
+    }
+
+    let listBaru = document.createElement("li");
+    let infoTugas = document.createElement("div");
+
+    infoTugas.innerHTML = `
+        <strong>${teksTugas}</strong><br>
+        Tanggal: ${tanggalTugas}<br>
+        Status: <span class="status">${statusTugas}</span>
+    `;;
+
+
+
