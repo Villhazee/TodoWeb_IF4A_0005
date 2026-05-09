70032c74e4ffea5bd3688a23650800aeb6bb262b
Arfantta <ohmygenius6@gmail.com>
2026-05-09 18:23:52 +0700

    menghubungkan element java script

---
+++ script.js
@@ -0,0 +1,5 @@
+const inputTugas = document.getElementById("inputTugas");
+const inputTanggal = document.getElementById("inputTanggal");
+const inputStatus = document.getElementById("inputStatus");
+const btnTambah = document.getElementById("btnTambah");
+const daftarTugas = document.getElementById("daftarTugas");
